17afed57722ffca124e70e9ede8dc9f9b261a8b7
ВАЛЕРИЙ <zaichkovv@mail.ru>
2020-12-08 18:29:12 +0300

    Добавляет изменения в header

--- source/js/header-open.js
+++ source/js/header-open.js
@@ -4,17 +4,7 @@ let menuToggle = header.querySelector(".header__menu-toggle");
 
 menuToggle.addEventListener("click", function (evt) {
   evt.preventDefault();
-  if (menuToggle.classList.contains("header__menu-toggle--opened")) {
-    menuToggle.classList.add("header__menu-toggle--closed");
-    menuToggle.classList.remove("header__menu-toggle--opened");
-    header.classList.remove("header--opened");
-    headerMenu.classList.remove("header__menu--opened");
-    headerMenu.classList.add("header__menu--closed");
-  } else {
-    menuToggle.classList.remove("header__menu-toggle--closed");
-    menuToggle.classList.add("header__menu-toggle--opened");
-    header.classList.add("header--opened");
-    headerMenu.classList.add("header__menu--opened");
-    headerMenu.classList.remove("header__menu--closed");
-  }
+  header.classList.toggle("header--opened");
+  headerMenu.classList.toggle("header__menu--opened");
+  menuToggle.classList.toggle("header__menu-toggle--opened");
 })
